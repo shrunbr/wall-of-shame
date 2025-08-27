50ecaf1e744c20c809ab872ad477bc1feb6fa59b
shrunbr <shrunbr@throne.dev>
2025-08-27 11:55:45 -0500

    enables full httpx logging

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ import uvicorn
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-logging.getLogger("httpx").setLevel(logging.WARNING)
+#logging.getLogger("httpx").setLevel(logging.WARNING)
 
 @asynccontextmanager
 async def lifespan(app):
